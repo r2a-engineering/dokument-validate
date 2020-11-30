cd438f73b3a5400367bef7ffaf591c8ffe513aa2
Ariel Reis <ariel@qrpoint.com.br>
2020-11-30 18:53:12 -0300

    test: cnpj improve tests

--- src/test/kotlin/br/eng/r2a/dokument/documents/brazil/CNPJTest.kt
+++ src/test/kotlin/br/eng/r2a/dokument/documents/brazil/CNPJTest.kt
@@ -16,4 +16,29 @@ internal class CNPJTest {
         val dkmntVldtr = DokumentValidator("24626495000127", CNPJ)
         assert(dkmntVldtr.validate())
     }
+
+
+    @Test
+    fun `when cnpj last digit is invalid, must return false`() {
+        val dkmntVldtr = DokumentValidator("24626495000128", CNPJ)
+        assert(!dkmntVldtr.validate())
+    }
+
+    @Test
+    fun `when cnpj last but one digit is invalid, must return false`() {
+        val dkmntVldtr = DokumentValidator("24626495000128", CNPJ)
+        assert(!dkmntVldtr.validate())
+    }
+
+    @Test
+    fun `when cnpj length is invalid, must return false`() {
+        val dkmntVldtr = DokumentValidator("2462649500012", CNPJ)
+        assert(!dkmntVldtr.validate())
+    }
+
+    @Test
+    fun `when cnpj is one of defaults, must return false`() {
+        val dkmntVldtr = DokumentValidator("00000000000000", CNPJ)
+        assert(!dkmntVldtr.validate())
+    }
 }
